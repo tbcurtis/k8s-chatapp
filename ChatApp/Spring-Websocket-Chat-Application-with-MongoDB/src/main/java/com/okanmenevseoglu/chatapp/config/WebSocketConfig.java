807b7b8411a630f13f39e72c2b9cec7f10a7f331
tbcurtis <tbcurtis20@gmail.com>
2022-09-22 13:29:12 -0700

    Working rabbitmq

--- ChatApp/Spring-Websocket-Chat-Application-with-MongoDB/src/main/java/com/okanmenevseoglu/chatapp/config/WebSocketConfig.java
+++ ChatApp/Spring-Websocket-Chat-Application-with-MongoDB/src/main/java/com/okanmenevseoglu/chatapp/config/WebSocketConfig.java
@@ -1,24 +1,53 @@
 package com.okanmenevseoglu.chatapp.config;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
+@EnableRabbit
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Value("${spring.rabbitmq.host}")
+    String host;
+
+    @Value("${spring.rabbitmq.username}")
+    String username;
+
+    @Value("${spring.rabbitmq.password}")
+    String password;
+
+    public static final String TOPIC_DESTINATION_PREFIX = "/topic";
+    public static final String REGISTRY = "/ws";
+        
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        
         registry.addEndpoint("/ws-chatapp").withSockJS();
 
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay(TOPIC_DESTINATION_PREFIX)
+        .setRelayHost(host)
+        .setClientLogin(username)
+        .setClientPasscode(password)
+        .setSystemLogin(username)
+        .setSystemPasscode(password);
+
+        //config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 }
